7065958e11d094ec0e387bc15c5c3c6b8e397db7
ChakriSriSaiPavan <145587697+ChakriSriSaiPavan@users.noreply.github.com>
2026-07-18 14:40:26 -0500

    Add Code deployed-1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "CI/CD Pipeline using AWS CodePipeline, CodeBuild, Docker & ECS";
+        return "CI/CD Pipeline Successfully Updated!";
     }
 
 }
